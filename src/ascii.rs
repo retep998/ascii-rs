6ba4409a2365379c4dbd3be2134c7af911ab4989
Peter Atashian <retep998@gmail.com>
2014-07-14 01:04:41 -0400

    sRGB to linear conversions Need to cleanup and modularize the main code.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/ascii.rs
+++ src/ascii.rs
@@ -1,11 +1,41 @@
 
 extern crate libc;
+extern crate term;
 
 use image::Image;
+use image::Table;
+use image::Color;
+use std::os::args;
+use std::io::stdio::stdin;
+use term::stdout;
 
 mod image;
 
-
 fn main() {
-    let img = Image::load(&Path::new("RainbowDashFluff.png"));
+    let args = args();
+    let table = Table::generate();
+    let max_width = 100u;
+    let char_width = 7u;
+    let char_height = 14u;
+    let chars_per_row = max_width / 2 - 1;
+    let mut cout = stdout().unwrap();
+    let mut cin = stdin();
+    let chars = include_str!("chars.txt");
+    if args.len() > 1 {
+        let chars_img = Image::load(&Path::new("chars.png")).unwrap();
+        let chars_img = chars_img.to_linear(&table, Color { r: 0, g: 0, b: 0, a: 0 });
+        let img = Image::load(&Path::new(args.get(1).as_slice())).unwrap();
+        let img = img.to_linear(&table, Color { r: 0, g: 0, b: 0, a: 0 });
+        println!("[{}, {}]", img.width(), img.height());
+    } else {
+        cout.fg(15).unwrap();
+        cout.bg(0).unwrap();
+        for (i, c) in chars.chars().enumerate() {
+            if i % chars_per_row == 0 {
+                writeln!(cout, "");
+            }
+            write!(cout, " {}", c);
+        }
+    }
+    cin.read_line().unwrap();
 }
--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,6 @@
 
 use libc::{c_uchar, c_uint, size_t, free, c_void};
+use std::cell::Cell;
 use std::io::fs::File;
 use std::mem::uninitialized;
 use std::slice::raw::buf_as_slice;
@@ -13,21 +14,34 @@ extern {
                         insize: size_t) -> c_uint;
 }
 
-struct Srgb;
-struct Linear;
-
 #[deriving(Show)]
 pub struct Color<T> {
-    r: T,
-    g: T,
-    b: T,
-    a: T,
+    pub r: T,
+    pub g: T,
+    pub b: T,
+    pub a: T,
+}
+
+impl Color<u8> {
+    fn to_linear(&self, table: &Table) -> Color<f32> {
+        Color {
+            r: table.r[self.r as uint],
+            g: table.g[self.g as uint],
+            b: table.b[self.b as uint],
+            a: table.a[self.a as uint],
+        }
+    }
 }
 
 pub struct Image<T> {
     width: u32,
     height: u32,
-    pixels: Vec<Color<T>>,
+    pixels: Vec<Cell<Color<T>>>,
+}
+
+impl<T> Image<T> {
+    pub fn width(&self) -> u32 { self.width }
+    pub fn height(&self) -> u32 { self.height }
 }
 
 impl Image<u8> {
@@ -45,11 +59,57 @@ impl Image<u8> {
             0 => (),
             _ => return Err("Failed to decode png data"),
         }
-        let pixels = unsafe { buf_as_slice(outbuf as *const Color<u8>, (width * height) as uint,
-                                           |decoded| decoded.iter().map(|&c| c).collect()) };
+        let pixels = unsafe {
+            buf_as_slice(outbuf as *const Color<u8>, (width * height) as uint,
+                         |decoded| decoded.iter().map(|&c| Cell::new(c)).collect())
+        };
         unsafe { free(outbuf as *mut c_void) };
         Ok(Image {
             width: width, height: height, pixels: pixels,
         })
+    }
+    pub fn to_linear(&self, table: &Table, back: Color<u8>) -> Image<f32> {
+        let back = back.to_linear(table);
+        let pixels = self.pixels.iter().map(|pixel| {
+            let pixel = pixel.get().to_linear(table);
+            Cell::new(Color {
+                r: pixel.r * pixel.a + back.r * (1. - pixel.a),
+                g: pixel.g * pixel.a + back.g * (1. - pixel.a),
+                b: pixel.b * pixel.a + back.b * (1. - pixel.a),
+                a: 1.
+            })
+        }).collect();
+        Image { width: self.width, height: self.height, pixels: pixels }
+    }
+}
+
+pub struct Table {
+    r: [f32, ..0x100],
+    g: [f32, ..0x100],
+    b: [f32, ..0x100],
+    a: [f32, ..0x100],
+}
+
+impl Table {
+    pub fn generate() -> Table {
+        fn srgb_to_linear(x: f32) -> f32 {
+            if x <= 0.04045 {
+                x / 12.92
+            } else {
+                ((x + 0.055) / (1. + 0.055)).powf(2.4)
+            }
+        }
+        let mut r: [f32, ..0x100] = unsafe { uninitialized() };
+        let mut g: [f32, ..0x100] = unsafe { uninitialized() };
+        let mut b: [f32, ..0x100] = unsafe { uninitialized() };
+        let mut a: [f32, ..0x100] = unsafe { uninitialized() };
+        for i in range(0u, 0x100) {
+            let val = srgb_to_linear(i as f32 / 255.);
+            r[i] = val * 0.2126;
+            g[i] = val * 0.7152;
+            b[i] = val * 0.0722;
+            a[i] = val;
+        }
+        Table { r: r, g: g, b: b, a: a }
     }
 }
